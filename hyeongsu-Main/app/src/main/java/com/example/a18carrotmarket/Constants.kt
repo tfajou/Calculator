25a2977af0f06f5174ab379ba26324358aa37b6b
tfajou <0523rla@naver.com>
2024-01-12 20:12:20 +0900

    notification and delete add code

---
+++ hyeongsu-Main/app/src/main/java/com/example/a18carrotmarket/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.a18carrotmarket
+
+object Constants {
+    const val ITEM_INDEX = "item_index"
+    const val ITEM_OBJECT = "item_object"
+}
---
+++ hyeongsu-Main/app/src/main/java/com/example/a18carrotmarket/DetailActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a18carrotmarket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.a18carrotmarket.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val itemName = intent.getStringExtra("ITEM_NAME")
+        val itemPrice = intent.getIntExtra("ITEM_PRICE", 0)
+        val itemLocate = intent.getStringExtra("ITEM_LOCATE")
+
+
+    }
+}
